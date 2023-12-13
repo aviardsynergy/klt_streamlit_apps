589227fcc4d80a9c408e6e0ba4cf5ef15f4ecb11
aviardsynergy <145564057+aviardsynergy@users.noreply.github.com>
2023-12-13 11:07:36 +0100

    Update klt_streamlit.py

--- klt_streamlit.py
+++ klt_streamlit.py
@@ -2,6 +2,10 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+from matplotlib.ticker import FuncFormatter
+
+def millions_formatter(x, pos):
+    return f'{x / 1e6}M'
 
 st.markdown("<h3 style='text-align: center;'>Nouvelle Prod : Comparaison Prédiction (Jour) et Réel (Fin de Mois)</h3> <br>", unsafe_allow_html=True)
 
@@ -19,16 +23,17 @@ else:
     df_filtre = df 
 
 fig, ax = plt.subplots()
-ax.plot(df_filtre['date'], df_filtre['REEL_NPROD'], label='N Prod')
-ax.plot(df_filtre['date'], df_filtre['PRED_NPROD'], label='Pred NProd')
+ax.plot(df_filtre['date'], df_filtre['REEL_NPROD'], label='N Prod', color='#003f5c')
+ax.plot(df_filtre['date'], df_filtre['PRED_NPROD'], label='Pred NProd', color='#bc5090')
 ax.set_xlabel('Date')
-ax.set_ylabel('Nouvelle Prod')
+ax.set_ylabel('Nouvelle Prod (en Millions)')
 ax.set_title('Nouvelle Prod Réelle et Prédite')
 
 ax.xaxis.set_major_locator(mdates.AutoDateLocator())
 ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
 
-# Rotation des étiquettes de l'axe des X
+ax.yaxis.set_major_formatter(FuncFormatter(millions_formatter))
+
 plt.setp(ax.get_xticklabels(), rotation=45, ha='right') 
 
 ax.legend()

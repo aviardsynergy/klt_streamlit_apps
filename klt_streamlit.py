6a3d96f8b4d85669604725cba83834b3c80efde5
aviardsynergy <145564057+aviardsynergy@users.noreply.github.com>
2023-12-13 10:50:34 +0100

    Update klt_streamlit.py

--- klt_streamlit.py
+++ klt_streamlit.py
@@ -20,7 +20,7 @@ ax.plot(df_filtre['date'], df_filtre['REEL_NPROD'], label='N Prod')
 ax.plot(df_filtre['date'], df_filtre['PRED_NPROD'], label='Pred NProd')
 ax.set_xlabel('Date')
 ax.set_ylabel('Nouvelle Prod')
-ax.set_title(f'Nouvelle Prod Réelle et Prédite pour {mois_choisi}')
+ax.set_title('Nouvelle Prod Réelle et Prédite')
 ax.legend()
 
 st.pyplot(fig)

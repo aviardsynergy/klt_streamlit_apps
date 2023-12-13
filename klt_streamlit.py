ebe0cc8c0c2d52edfbc9bc32e2f6516e77b7b47a
aviardsynergy <145564057+aviardsynergy@users.noreply.github.com>
2023-12-13 14:17:09 +0100

    Update klt_streamlit.py

--- klt_streamlit.py
+++ klt_streamlit.py
@@ -1,11 +1,7 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
 from matplotlib.ticker import FuncFormatter
-from math import pi
-
 
 def millions_formatter(x, pos):
     return f'{x / 1e6}M'
@@ -42,32 +38,3 @@ plt.setp(ax.get_xticklabels(), rotation=45, ha='right')
 
 ax.legend()
 st.pyplot(fig)
-
-
-
-
-categories = df_filtre['mois'].unique()
-N = len(categories)
-
-if N > 0:
-    try:
-        # Calcul des valeurs moyennes pour chaque mois
-        avg_reel = df_filtre.groupby('mois')['REEL_NPROD'].mean().reindex(categories, fill_value=0)
-        avg_pred = df_filtre.groupby('mois')['PRED_NPROD'].mean().reindex(categories, fill_value=0)
-
-        # Préparation des angles pour le graphique radar
-        angles = np.linspace(0, 2 * pi, N, endpoint=False).tolist()
-        angles += angles[:1]  # Fermeture du cercle
-
-        # Création du graphique radar
-        radar_fig, ax = plt.subplots(figsize=(8, 8), subplot_kw=dict(polar=True))
-        ax.plot(angles, avg_reel.tolist() + avg_reel.tolist()[:1], linewidth=2, linestyle='solid', label='REEL_NPROD')
-        ax.plot(angles, avg_pred.tolist() + avg_pred.tolist()[:1], linewidth=2, linestyle='solid', label='PRED_NPROD')
-        ax.set_thetagrids(np.degrees(angles[:-1]), categories)
-        ax.legend(loc='upper right', bbox_to_anchor=(0.1, 0.1))
-
-        st.pyplot(radar_fig)
-    except Exception as e:
-        st.error(f"Une erreur s'est produite lors de la création du graphique radar: {e}")
-else:
-    st.write("Aucun mois sélectionné ou aucune donnée disponible pour les mois sélectionnés.")
